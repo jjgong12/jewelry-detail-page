380e8259cd2c13e8a5d81e1346d96b53b6bf78ed
jjgong12 <65600544+jjgong12@users.noreply.github.com>
2025-07-31 14:54:10 +0900

    Update handler.py

--- handler.py
+++ handler.py
@@ -11,6 +11,7 @@ import json
 import traceback
 
 
+
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
